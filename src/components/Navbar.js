734b94b6e31dc46b08155de1bc7f0a0610e2abb3
santiago Morelli <santiagomorelli@gmail.com>
2022-03-20 13:44:57 +1100

    change navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../logo.svg';
-import { FaCocktail } from 'react-icons/fa';
+import { FaCocktail, FaBook } from 'react-icons/fa';
+
 import SearchForm from '../components/SearchForm';
 
 export default function Navbar() {
   return (
     <nav className='navbar'>
       <div className='nav-center'>
+        <SearchForm />
         <Link to='/'>
           <h1 className='logo'>
             Lets go for a <FaCocktail />
@@ -16,13 +18,11 @@ export default function Navbar() {
             <FaCocktail />
           </h1>
         </Link>
-        <SearchForm />
         <ul className='nav-links'>
           <li>
-            <Link to='/'>home</Link>
-          </li>
-          <li>
-            <Link to='/about'>about</Link>
+            <Link to='/about'>
+              <FaBook />
+            </Link>
           </li>
         </ul>
       </div>
