24a9064a8dbdfa5373405389feb5d7cea1e09efd
santiago Morelli <santiagomorelli@gmail.com>
2022-03-21 19:40:06 +1100

    change navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,24 +8,31 @@ import SearchForm from '../components/SearchForm';
 export default function Navbar() {
   return (
     <nav className='navbar'>
-      <div className='nav-center'>
-        <SearchForm />
-        <Link to='/'>
-          <h1 className='logo'>
-            Lets go for a <FaCocktail />
-          </h1>
-          <h1 className='icon'>
-            <FaCocktail />
-          </h1>
-        </Link>
-        <ul className='nav-links'>
-          <li>
-            <Link to='/about'>
+      <nav className='nav-center'>
+        <div>
+          <SearchForm />
+        </div>
+        <div>
+          <Link to='/'>
+            <h1 className='logo'>
+              Lets go for a <FaCocktail />
+            </h1>
+            <h1 className='icon'>
+              <FaCocktail />
+            </h1>
+          </Link>
+        </div>
+        <div>
+          <Link to='/about'>
+            <h1 className='logo'>
+              About <FaBook />
+            </h1>
+            <h1 className='icon'>
               <FaBook />
-            </Link>
-          </li>
-        </ul>
-      </div>
+            </h1>
+          </Link>
+        </div>
+      </nav>
     </nav>
   );
 }
